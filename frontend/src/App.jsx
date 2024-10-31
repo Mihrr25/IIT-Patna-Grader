2c586940b9e9d419d8dc8a04faa5b0903aa119ad
Mihir Bairathi <mihirbairathi25@gmail.com>
2024-10-31 14:44:41 +0530

    intervchanging links for backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     formdata.append("file",myFile)
 
     try {
-      const response = await fetch("http://localhost:5000/upload", {
+      const response = await fetch("https://renderiitpgraderproject.onrender.com/upload", {
         method: "POST",
         body: formdata,
       });
